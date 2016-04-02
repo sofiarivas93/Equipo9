a088bde26e62e53285639f08d547c7c90115e320
LeticiaMartinez <letuumarti@gmail.com>
2016-04-02 11:34:06 -0400

    Revert "LeticiaMartinez"
    This reverts commit b5a932a9829c57525e5b5e7c5156bcab9a32d2fa.

--- GestionP/RegistroApp/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index_view, name='RegistroApp.index'),
-    url(r'^login/$', views.login_view, name='RegistroApp.login'),
-]
